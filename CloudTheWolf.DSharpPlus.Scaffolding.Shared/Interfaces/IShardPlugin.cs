03f917a9c797dbb23ba1af4e47aaff4f0b31a2ef
CloudTheWolf <ukkmmichael@gmail.com>
2022-10-22 12:40:12 +0100

    [Interface\I*Plugin] - Yes

--- CloudTheWolf.DSharpPlus.Scaffolding.Shared/Interfaces/IShardPlugin.cs
+++ CloudTheWolf.DSharpPlus.Scaffolding.Shared/Interfaces/IShardPlugin.cs
@@ -1,28 +1,12 @@
-﻿using CloudTheWolf.DSharpPlus.Scaffolding.Logging;
-using DSharpPlus;
+﻿using DSharpPlus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using CloudTheWolf.DSharpPlus.Scaffolding.Logging;
 
 namespace CloudTheWolf.DSharpPlus.Scaffolding.Shared.Interfaces
 {
-    /// <summary>
-    /// Plugin Interface
-    /// </summary>
-    public interface IPlugin
+    internal interface IShardPlugin : IPlugin
     {
-        /// <summary>
-        /// Module Name
-        /// </summary>
-        string Name { get; }
-        /// <summary>
-        /// Module Description
-        /// </summary>
-        string Description { get; }
-        /// <summary>
-        /// Modude Version
-        /// </summary>
-        int Version { get; }
-
         /// <summary>
         /// Initilize Plugin Module
         /// </summary>
@@ -31,6 +15,5 @@ namespace CloudTheWolf.DSharpPlus.Scaffolding.Shared.Interfaces
         /// <param name="discordConfiguration"></param>
         /// <param name="applicationConfig"></param>
         void InitPlugin(IBot bot, ILogger<Logger> logger, DiscordConfiguration discordConfiguration, IConfigurationRoot applicationConfig);
-
     }
 }
