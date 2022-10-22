6dfc25e5564247e53d5334d62e42681b953161b8
CloudTheWolf <ukkmmichael@gmail.com>
2022-10-22 09:56:44 +0100

    [Interfaces\IShardBot] - Create new interface for Sharding

---
+++ CloudTheWolf.DSharpPlus.Scaffolding.Shared/Interfaces/IShardBot.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using System.Collections.Generic;
+
+namespace CloudTheWolf.DSharpPlus.Scaffolding.Shared.Interfaces
+{
+    /// <summary>
+    /// Bot Interface For Bots Using Sharding
+    /// </summary>
+    public interface IShardBot
+    {
+
+        /// <summary>
+        /// <see cref="DiscordShardedClient"/>
+        /// </summary>
+        DiscordShardedClient Client { get; set; }
+
+        /// <summary>
+        /// <see cref="DiscordRestClient"/>
+        /// </summary>
+        DiscordRestClient Rest { get; set; }
+
+        /// <summary>
+        ///<see cref="CommandsNextExtension"/>
+        /// </summary>
+        IReadOnlyDictionary<int, CommandsNextExtension> Commands { get; set;}
+        /// <summary>
+        /// <see cref="InteractivityExtension"/>
+        /// </summary>
+        IReadOnlyDictionary<int, InteractivityExtension> Interactivity { get; set; }
+
+        /// <summary>
+        /// <see cref="VoiceNextExtension"/>
+        /// </summary>
+        IReadOnlyDictionary<int, VoiceNextExtension> Voice { get; set; }
+
+        /// <summary>
+        /// <see cref="InteractivityExtension"/>
+        /// </summary>
+        IReadOnlyDictionary<int, LavalinkConfiguration> LavalinkConfig { get; set; }
+
+        /// <summary>
+        /// <see cref="SlashCommandsExtension"/>
+        /// </summary>
+        IReadOnlyDictionary<int, SlashCommandsExtension> SlashCommandsExt { get; set; }
+    }
+
+}
