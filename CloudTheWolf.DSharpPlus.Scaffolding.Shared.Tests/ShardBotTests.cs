26616da60da65b54ee577eb97fb727a97cb9c8da
CloudTheWolf <ukkmmichael@gmail.com>
2023-04-11 23:27:12 +0100

    project/tests - Add Unit Testing

---
+++ CloudTheWolf.DSharpPlus.Scaffolding.Shared.Tests/ShardBotTests.cs
@@ -0,0 +1,76 @@
+namespace CloudTheWolf.DSharpPlus.Scaffolding.Shared.Tests
+{
+    using global::DSharpPlus;
+    using Microsoft.Extensions.Logging;
+
+    public class ShardBot : IShardBot
+    {
+        public DiscordShardedClient Client { get; set; }
+
+        public DiscordRestClient Rest { get; set; }
+
+        public IReadOnlyDictionary<int, CommandsNextExtension> Commands { get; set; }
+
+        public IReadOnlyDictionary<int, InteractivityExtension> Interactivity { get; set; }
+
+        public IReadOnlyDictionary<int, VoiceNextExtension> Voice { get; set; }
+
+        public IReadOnlyDictionary<int, LavalinkConfiguration> LavalinkConfig { get; set; }
+
+        public IReadOnlyDictionary<int, SlashCommandsExtension> SlashCommandsExt { get; set; }
+
+        public static DiscordConfiguration DiscordConfig = new ()
+                                                               {
+                                                                   Token = "super-secret-token-owo",
+                                                                   TokenType = TokenType.Bot,
+                                                                   AutoReconnect = true,
+                                                                   MinimumLogLevel = LogLevel.Debug,
+                                                               };
+    }
+
+    [TestFixture]
+    public class ShardBotTests
+    {
+        [Test]
+        public void Test_ValidateSharedBot()
+        {
+            // Arrange
+            var client = new DiscordShardedClient(ShardBot.DiscordConfig);
+            var rest = new DiscordRestClient(ShardBot.DiscordConfig);
+            var commands = new Dictionary<int, CommandsNextExtension>();
+            var interactivity = new Dictionary<int, InteractivityExtension>();
+            var voice = new Dictionary<int, VoiceNextExtension>();
+            var lavalink = new Dictionary<int, LavalinkConfiguration>();
+            var slashCommands = new Dictionary<int, SlashCommandsExtension>();
+
+            var bot = new ShardBot
+                          {
+                              Client = client,
+                              Rest = rest,
+                              Commands = commands,
+                              Interactivity = interactivity,
+                              Voice = voice,
+                              LavalinkConfig = lavalink,
+                              SlashCommandsExt = slashCommands
+                          };
+
+            // Act
+            var actualClient = bot.Client;
+            var actualRest = bot.Rest;
+            var actualCommands = bot.Commands;
+            var actualInteractivity = bot.Interactivity;
+            var actualVoice = bot.Voice;
+            var actualLavalink = bot.LavalinkConfig;
+            var actualSlashCommands = bot.SlashCommandsExt;
+
+            // Assert
+            Assert.AreEqual(client, actualClient);
+            Assert.AreEqual(rest, actualRest);
+            Assert.AreEqual(commands, actualCommands);
+            Assert.AreEqual(interactivity, actualInteractivity);
+            Assert.AreEqual(voice, actualVoice);
+            Assert.AreEqual(lavalink, actualLavalink);
+            Assert.AreEqual(slashCommands, actualSlashCommands);
+        }
+    }
+}
---
+++ CloudTheWolf.DSharpPlus.Scaffolding.Shared.Tests/Usings.cs
@@ -0,0 +1,10 @@
+
+global using DSharpPlus;
+global using DSharpPlus.Interactivity;
+global using DSharpPlus.Lavalink;
+global using DSharpPlus.SlashCommands;
+global using Moq;
+global using NUnit.Framework;
+global using DSharpPlus.CommandsNext;
+global using DSharpPlus.VoiceNext;
+global using CloudTheWolf.DSharpPlus.Scaffolding.Shared.Interfaces;
